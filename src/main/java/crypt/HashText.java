63d335d0d902f52c183cdcdc37e07854f6980546
Nikitin Ilya <nikitin_ilya@outlook.com>
2016-04-19 23:24:50 +0500

    add class HashText with empty methods

---
+++ src/main/java/crypt/HashText.java
@@ -0,0 +1,11 @@
+package crypt;
+
+/**
+ * Created by User on 19.04.2016.
+ */
+public class HashText {
+    //todo class HashText
+    public static String getHash(String pass, Object salt) {
+        return null;
+    }
+}
