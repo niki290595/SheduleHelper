847563e33a5f9c35a5f0a8e5c1beb49d18edf502
Nikitin Ilya <nikitin_ilya@outlook.com>
2016-04-19 23:24:38 +0500

    add class RegistrationViewController with empty methods

---
+++ src/main/java/controllers/RegistrationViewController.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import javafx.stage.Stage;
+
+/**
+ * Created by User on 19.04.2016.
+ */
+public class RegistrationViewController {
+    //todo class RegistrationViewController
+
+    public RegistrationViewController(Stage stage) {
+
+    }
+}
