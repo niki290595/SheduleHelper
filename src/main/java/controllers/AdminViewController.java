6023b6b88441ef0af139f1248ecd7525ca50b693
Nikitin Ilya <nikitin_ilya@outlook.com>
2016-04-19 23:24:10 +0500

    add class CreatePassViewController with empty methods

---
+++ src/main/java/controllers/AdminViewController.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import javafx.stage.Stage;
+
+/**
+ * Created by User on 19.04.2016.
+ */
+public class AdminViewController {
+    //todo class AdminViewController
+
+    public AdminViewController(Stage stage, String login) {
+
+    }
+}
