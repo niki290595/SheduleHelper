48e374b6ca65a8c0f00e986060dff221a88dbe50
Nikitin Ilya <nikitin_ilya@outlook.com>
2016-04-19 23:24:19 +0500

    add class CreatePassViewController with empty methods

---
+++ src/main/java/controllers/CreatePassViewController.java
@@ -0,0 +1,20 @@
+package controllers;
+
+import entity.UserEntity;
+import javafx.stage.Stage;
+
+/**
+ * Created by User on 19.04.2016.
+ */
+public class CreatePassViewController {
+    private static String pass;
+    //todo class CreatePassViewController
+
+    public CreatePassViewController(Stage stage, UserEntity userEntity) {
+
+    }
+
+    public static String getPass() {
+        return pass;
+    }
+}
