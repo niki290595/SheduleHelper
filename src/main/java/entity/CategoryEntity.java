0a65f24c2e4bdde9ae1426d317b36d4b6c1b0fdc
Nikitin Ilya <nikitin_ilya@outlook.com>
2016-04-20 23:01:39 +0500

    add CategoryEntity class implementation

--- src/main/java/entity/CategoryEntity.java
+++ src/main/java/entity/CategoryEntity.java
@@ -1,12 +1,15 @@
 package entity;
 
-/**
- * Created by User on 19.04.2016.
- */
-public class CategoryEntity {
-    //todo class CategoryEntity
+import javax.persistence.*;
 
-    public enum CategoryType {
+/**
+ * Created by User on 20.04.2016.
+ */
+@Entity
+@Table(name = "category", schema = "scheduledb")
+public class CategoryEntity implements Comparable<CategoryEntity> {
+
+    public static enum CategoryType {
         ADMIN, OWNER, STAFF;
 
         static int admin = "admin".hashCode();
@@ -22,7 +25,62 @@ public class CategoryEntity {
         }
     }
 
+    private Integer id;
+    private String description;
+
+    @Id
+    @Column(name = "id")
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "description")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public CategoryType categoryType() {
+        int hashCode = this.getDescription().hashCode();
+        if (hashCode == CategoryType.admin) return CategoryType.ADMIN;
+        if (hashCode == CategoryType.owner) return CategoryType.OWNER;
+        if (hashCode == CategoryType.staff) return CategoryType.STAFF;
         return null;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CategoryEntity that = (CategoryEntity) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        return description != null ? description.equals(that.description) : that.description == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return description;
+    }
+
+    public int compareTo(CategoryEntity o) {
+        return this.getDescription().compareTo(o.getDescription());
     }
 }
