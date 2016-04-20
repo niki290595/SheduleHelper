b3c1059f7d5ce633cbb2aca955d08fa9b5fbef2b
Nikitin Ilya <nikitin_ilya@outlook.com>
2016-04-20 23:24:04 +0500

    add DisciplineTypeEntity class implementation

---
+++ src/main/java/entity/DisciplineTypeEntity.java
@@ -0,0 +1,53 @@
+package entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by User on 20.04.2016.
+ */
+@Entity
+@Table(name = "disciplinetype", schema = "scheduledb")
+public class DisciplineTypeEntity {
+    private Integer id;
+    private String name;
+
+    @Id
+    @Column(name = "id")
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        DisciplineTypeEntity that = (DisciplineTypeEntity) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+}
