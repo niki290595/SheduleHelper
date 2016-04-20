97e3373aa3a2af0979c7b6271bf97fe365177ae6
Nikitin Ilya <nikitin_ilya@outlook.com>
2016-04-20 22:22:12 +0500

    add HibernateUtil for SessionBuilding

---
+++ src/main/java/orm/HibernateUtil.java
@@ -0,0 +1,32 @@
+package orm;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Created by User on 20.04.2016.
+ */
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory;
+
+    static {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        try {
+            sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static Session getSession() {
+        return sessionFactory.openSession();
+    }
+}
