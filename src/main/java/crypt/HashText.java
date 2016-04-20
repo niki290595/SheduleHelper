f8c5312e78aab06b5654c47c78f841e0db50d50b
Nikitin Ilya <nikitin_ilya@outlook.com>
2016-04-20 22:06:53 +0500

    add HashText class implementation

--- src/main/java/crypt/HashText.java
+++ src/main/java/crypt/HashText.java
@@ -1,11 +1,31 @@
 package crypt;
 
-/**
- * Created by User on 19.04.2016.
- */
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class HashText {
-    //todo class HashText
-    public static String getHash(String pass, Object salt) {
-        return null;
+    public static String getHash(String text, String salt) {
+        String s = text + salt;
+        return sha512(s);
+    }
+
+    private static String sha512(String str) {
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA-512");
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("algorithm sha-512 non found");
+            System.exit(-1);
+        }
+        md.update(str.getBytes());
+        byte byteData[] = md.digest();
+
+        //convert the byte to hex format method 1
+        StringBuffer hashCodeBuffer = new StringBuffer();
+        for (int i = 0; i < byteData.length; i++) {
+            hashCodeBuffer.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+        }
+        return hashCodeBuffer.toString();
     }
 }
