1b08928faac1668ee169371b821b95b2370b4b79
Nikitin Ilya <nikitin_ilya@outlook.com>
2016-04-21 11:09:51 +0500

    add empty UserEditor class

---
+++ src/main/java/controllers/UserEditorViewController.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import entity.UserEntity;
+import javafx.stage.Stage;
+
+/**
+ * Created by User on 21.04.2016.
+ */
+public class UserEditorViewController {
+    //todo class UserEditorViewController
+    public UserEditorViewController(Stage stage) {
+    }
+
+    public UserEditorViewController(Stage stage, UserEntity user) {
+
+    }
+}
