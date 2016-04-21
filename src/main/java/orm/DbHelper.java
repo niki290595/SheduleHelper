d3f2d7b44aab999ad452808b53d72e02b18b0f4c
Nikitin Ilya <nikitin_ilya@outlook.com>
2016-04-21 13:26:51 +0500

    add DbHelper class implementation

---
+++ src/main/java/orm/DbHelper.java
@@ -0,0 +1,81 @@
+package orm;
+
+import entity.CategoryEntity;
+import entity.UserEntity;
+import org.hibernate.Session;
+
+import java.sql.Date;
+import java.util.Calendar;
+import java.util.Collection;
+
+/**
+ * Created by User on 21.04.2016.
+ */
+public enum DbHelper {
+    INSTANCE;
+
+    public Collection getUsersData() {
+        try (Session session = HibernateGenericDao.getSessionFactory().openSession()) {
+            return session.createCriteria(UserEntity.class).list();
+        }
+    }
+
+    public UserEntity addUser(String login, String pass, CategoryEntity category, String salt) {
+        try (Session session = HibernateGenericDao.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            UserEntity user = new UserEntity(login, pass, category, salt, new Date(Calendar.getInstance().getTime().getTime()));
+            user.setId((Integer) session.save(user));
+            session.getTransaction().commit();
+            return user;
+        }
+    }
+
+    public UserEntity addUser(String login, CategoryEntity category) {
+        try (Session session = HibernateGenericDao.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            UserEntity user = new UserEntity();
+            user.setLogin(login);
+            user.setCategory(category);
+            user.setDateCreation(new Date(Calendar.getInstance().getTime().getTime()));
+            user.setId((Integer) session.save(user));
+            session.getTransaction().commit();
+            return user;
+        }
+    }
+
+    public UserEntity editUser(UserEntity user, String newLogin, CategoryEntity newCategory) {
+        return editUser(user, newLogin, null, newCategory, null);
+    }
+
+    public UserEntity editUser(UserEntity user, String pass, String salt) {
+        return editUser(user, null, pass, null, salt);
+    }
+
+    private UserEntity editUser(UserEntity user, String login, String pass, CategoryEntity category, String salt) {
+        try (Session session = HibernateGenericDao.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            user = session.get(UserEntity.class, user.getId());
+            if (login != null) user.setLogin(login);
+            if (pass != null) user.setPass(pass);
+            if (category != null) user.setCategory(category);
+            if (salt != null) user.setSalt(salt);
+            user.setDateModification(new java.sql.Date(Calendar.getInstance().getTime().getTime()));
+            session.getTransaction().commit();
+            return user;
+        }
+    }
+
+    public void removeUser(UserEntity user) {
+        try (Session session = HibernateGenericDao.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.delete(user);
+            session.getTransaction().commit();
+        }
+    }
+
+    public Collection getCategoryData() {
+        try (Session session = HibernateGenericDao.getSessionFactory().openSession()) {
+            return session.createCriteria(CategoryEntity.class).list();
+        }
+    }
+}
