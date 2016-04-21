7ccdb2cec4627166ae8b7837333bd8a2a67107da
Nikitin Ilya <nikitin_ilya@outlook.com>
2016-04-21 11:11:02 +0500

    add AdminViewController class implementation

--- src/main/java/controllers/AdminViewController.java
+++ src/main/java/controllers/AdminViewController.java
@@ -1,14 +1,79 @@
 package controllers;
 
+import entity.UserEntity;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import orm.Repository;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Date;
+import java.util.ResourceBundle;
 
 /**
  * Created by User on 19.04.2016.
  */
-public class AdminViewController {
-    //todo class AdminViewController
+public class AdminViewController implements Initializable {
+    private static Stage stage;
+    private static Repository db;
 
-    public AdminViewController(Stage stage, String login) {
+    static {
+        db = Repository.INSTANCE;
+    }
 
+    public TableView<UserEntity> usersTable;
+    public TableColumn<UserEntity, String> loginColumn;
+    public TableColumn<UserEntity, String> categoryColumn;
+    public TableColumn<UserEntity, Date> dateCreationColumn;
+    public TableColumn<UserEntity, Date> dateModificationColumn;
+
+    public AdminViewController() {}
+
+    public AdminViewController(Stage stage, String login) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("views/admin.form.fxml"));
+        stage = new Stage();
+        stage.setTitle("Добро пожаловать " + login);
+        stage.setScene(new Scene(root, 400, 200));
+        AdminViewController.stage = stage;
+        stage.show();
+        //AdminController.login = login;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        loginColumn.setCellValueFactory(new PropertyValueFactory<>("login"));
+        categoryColumn.setCellValueFactory(new PropertyValueFactory<>("category"));
+        dateCreationColumn.setCellValueFactory(new PropertyValueFactory<>("dateCreation"));
+        dateModificationColumn.setCellValueFactory(new PropertyValueFactory<>("dateModification"));
+        usersTable.setItems(db.getUserData());
+    }
+
+    public void addUser(ActionEvent actionEvent) throws IOException {
+        new UserEditorViewController(stage);
+    }
+
+    public void editUser(ActionEvent actionEvent) throws IOException {
+        UserEntity user = usersTable.getSelectionModel().getSelectedItem();
+        if (user != null) {
+            new UserEditorViewController(stage, user);
+        }
+    }
+
+    public void removeUser(ActionEvent actionEvent) {
+        UserEntity user = usersTable.getSelectionModel().getSelectedItem();
+        if (user != null) {
+            db.removeUser(user);
+        }
+    }
+
+    public void exit(ActionEvent actionEvent) {
+        System.exit(0);
     }
 }
