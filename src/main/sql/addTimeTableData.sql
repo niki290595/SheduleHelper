ce09e5d75857ff39b6dc47520f8be2dbd18b44af
Nikitin Ilya <nikitin_ilya@outlook.com>
2016-04-28 18:56:59 +0500

    add fill time table

---
+++ src/main/sql/addTimeTableData.sql
@@ -0,0 +1,18 @@
+use scheduledb;
+
+SELECT * FROM timetable;
+
+INSERT INTO timetable (id, timeBegin, timeEnd)
+	VALUES(1, '8:00', '9:00');
+INSERT INTO timetable (id, timeBegin, timeEnd)
+	VALUES(2, '9:00', '10:00');
+INSERT INTO timetable (id, timeBegin, timeEnd)
+	VALUES(3, '10:00', '11:00');
+INSERT INTO timetable (id, timeBegin, timeEnd)
+	VALUES(4, '11:00', '12:00');
+INSERT INTO timetable (id, timeBegin, timeEnd)
+	VALUES(5, '12:00', '13:00');
+INSERT INTO timetable (id, timeBegin, timeEnd)
+	VALUES(6, '13:00', '14:00');
+INSERT INTO timetable (id, timeBegin, timeEnd)
+	VALUES(7, '14:00', '15:00');
