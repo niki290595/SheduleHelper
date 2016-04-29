39414ba85edf5f0637ae7b98be861ef0bc9fe4c3
Nikitin Ilya <nikitin_ilya@outlook.com>
2016-04-29 12:12:25 +0500

    add time table entity collection

--- src/main/java/orm/Repository.java
+++ src/main/java/orm/Repository.java
@@ -2,16 +2,14 @@ package orm;
 
 import crypt.HashText;
 import crypt.SaltGenerator;
-import entity.CategoryEntity;
-import entity.UserEntity;
+import entity.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.ObservableMap;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.sql.Time;
+import java.util.*;
 
 /**
  * Created by User on 19.04.2016.
@@ -20,22 +18,27 @@ public enum Repository {
     INSTANCE;
 
     private static DbHelper dbHelper;
-    private ObservableList<UserEntity> userData;
-    private ObservableList<CategoryEntity> categoryData;
 
     static {
         dbHelper = DbHelper.INSTANCE;
     }
 
-    public ObservableList<UserEntity> getUserData() {
-        return userData == null ? initUserData() : userData;
-    }
+    private ObservableList<UserEntity> userData;
+    private ObservableList<CategoryEntity> categoryData;
+    private ObservableMap<Integer, TimeTableEntity> timeTableData;
+    private ObservableList<DirectionEntity> directionData;
+    private ObservableMap<DirectionEntity, ObservableList<GroupEntity>> directionGroupData;
 
     private ObservableList addCollection(Collection collection) {
         ObservableList observableList = FXCollections.observableArrayList();
         observableList.addAll(collection);
         Collections.sort(observableList);
         return observableList;
+    }
+
+    //region USER_ENTITY
+    public ObservableList<UserEntity> getUserData() {
+        return userData == null ? initUserData() : userData;
     }
 
     private ObservableList<UserEntity> initUserData() {
@@ -73,7 +76,6 @@ public enum Repository {
         Collections.sort(userData);
     }
 
-
     public void editUser(UserEntity user, String pass) {
         String salt = SaltGenerator.generate();
         UserEntity alterUser = dbHelper.editUser(user, HashText.getHash(pass, salt), salt);
@@ -86,7 +88,9 @@ public enum Repository {
         dbHelper.removeUser(user);
         userData.remove(user);
     }
+    //endregion
 
+    //region CATEGORY_ENTITY
     public ObservableList<CategoryEntity> getCategoryData() {
         return categoryData == null ? initCategoryData() : categoryData;
     }
@@ -95,4 +99,49 @@ public enum Repository {
         categoryData = addCollection(dbHelper.getCategoryData());
         return categoryData;
     }
+    //endregion
+
+    //region TIME_TABLE_ENTITY
+    public Map<Integer, TimeTableEntity> getTimeTableData() {
+        return timeTableData == null ? initTimeTableData() : timeTableData;
+    }
+
+    private Map<Integer, TimeTableEntity> initTimeTableData() {
+        timeTableData = FXCollections.observableHashMap();
+        Collection<TimeTableEntity> timeTableData = dbHelper.getTimeTableData();
+        for (TimeTableEntity item : timeTableData) {
+            this.timeTableData.put(item.getId(), item);
+        }
+        return this.timeTableData;
+    }
+    //endregion
+
+    //region DIRECTION_ENTITY
+    public ObservableList<DirectionEntity> getDirectionData() {
+        return directionData == null ? initDirectionData() : directionData;
+    }
+
+    private ObservableList<DirectionEntity> initDirectionData() {
+        directionData = addCollection(dbHelper.getDirectionData());
+        return directionData;
+    }
+    //endregion
+
+    //region GROUP_ENTITY
+    public ObservableList<GroupEntity> getGroupData(DirectionEntity direction) {
+        if (directionGroupData.get(direction) == null) {
+            initGroupData(direction);
+        }
+        return directionGroupData.get(direction);
+    }
+
+    private void initGroupData(DirectionEntity direction) {
+        ObservableList<GroupEntity> groupObservableList = addCollection(dbHelper.getGroupData());
+        directionGroupData.put(direction, groupObservableList);
+    }
+    //endregion
+
+    //region SCHEDULE_ENTITY
+    //endregion
+
 }
