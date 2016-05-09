6f708c07352c9082afd2ba8aea2ed2dff4ee31f2
Nikitin Ilya <nikitin_ilya@outlook.com>
2016-05-09 16:40:39 +0500

    add data for audience type table

---
+++ src/main/sql/addAudienceTypeData.sql
@@ -0,0 +1,6 @@
+USE scheduledb;
+
+INSERT INTO audiencetype (name) VALUES ('Для лекций');
+INSERT INTO audiencetype (name) VALUES ('Компьютерный класс');
+INSERT INTO audiencetype (name) VALUES ('Лаборатория');
+INSERT INTO audiencetype (name) VALUES ('Для практики');
