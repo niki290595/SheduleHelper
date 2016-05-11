c6fb7c179c83877866fc8c73de3b63bc8afca15b
Nikitin Ilya <nikitin_ilya@outlook.com>
2016-05-11 10:14:40 +0500

    fix bugs

---
+++ src/main/sql/addTeacherData.sql
@@ -0,0 +1,10 @@
+INSERT INTO teacher (FIO, academicdegree, position, phone)
+  VALUES ('Александров Васил Николов','Кандидат математических наук','Профессор','');
+INSERT INTO teacher (FIO, academicdegree, position, phone)
+  VALUES ('Алябъева Валентина Георгиевна','Кандидат физико-математических наук','Доцент','');
+INSERT INTO teacher (FIO, academicdegree, position, phone)
+  VALUES ('Андронов Михаил Александрович','','Старший преподаватель','');
+INSERT INTO teacher (FIO, academicdegree, position, phone)
+  VALUES ('Анисимов Александр Олегович','','Ассистент','');
+INSERT INTO teacher (FIO, academicdegree, position, phone)
+  VALUES ('Гадиатулин Роман Александрович','','Ассистент','');
