d1ea3c3e6c813ea7ebbe150748c24369b5b21968
Nikitin Ilya <nikitin_ilya@outlook.com>
2016-06-07 19:16:32 +0500

    add generic main panel

---
+++ src/main/java/controllers/generic/MainPanelViewController.java
@@ -0,0 +1,34 @@
+package controllers.generic;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by User on 07.06.2016.
+ */
+public class MainPanelViewController implements Initializable {
+
+    public MainPanelViewController() {
+    }
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
+
+
+    public void add(ActionEvent actionEvent) {
+    }
+
+    public void edit(ActionEvent actionEvent) {
+    }
+
+    public void del(ActionEvent actionEvent) {
+    }
+
+    public void exit(ActionEvent actionEvent) {
+    }
+}
